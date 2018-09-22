35d38ef8e0d39a0126b737229b2a2f9dee9861fa
dugagjin <lashi.dugagjin@gmail.com>
2018-09-22 16:55:22 +0200

    export was not correct. Could not use the package. Now it is ok.

--- source/index.ts
+++ source/index.ts
@@ -1,2 +1,6 @@
-export * from './network';
-export { subtract } from './helpers';
+import { default as nn } from './network';
+import { subtract } from './helpers';
+
+const Network = nn;
+
+export { Network, subtract };
